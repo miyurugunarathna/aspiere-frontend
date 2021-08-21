e630fcbb32bc6b2305a657643bc33d5f329a4d3f
miyurugunarathna <miyurupriyawadan@gmail.com>
2021-08-21 17:10:25 +0530

    to reslove conflict occurred

--- src/app/layout/dashboard/dashboard-routing.module.ts
+++ src/app/layout/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeesComponent } from 'src/app/components/fees/fees.component';
 import { PaymentComponent } from 'src/app/components/payment/payment.component';
+import { ViewPaymentComponent } from 'src/app/components/payment/view-payment/view-payment.component';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
@@ -12,6 +14,8 @@ const routes: Routes = [
         path: '',
         children: [
           { path: 'payment', component: PaymentComponent },
+          { path: 'payment/view', component: ViewPaymentComponent },
+          { path: 'fees', component: FeesComponent },
         ]
       }
     ]
