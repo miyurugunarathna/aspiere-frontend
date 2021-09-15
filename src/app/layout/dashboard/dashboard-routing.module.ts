c4c5288cade3ca74ba425c1a1341c2c2e2870d53
unknown <ravidubandaranayake99@gmail.com>
2021-09-15 19:44:13 +0530

    class and subject commits

--- src/app/layout/dashboard/dashboard-routing.module.ts
+++ src/app/layout/dashboard/dashboard-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClassComponent } from 'src/app/components/class/class.component';
 import { CoursesComponent } from 'src/app/components/courses/courses.component';
 import { PaymentComponent } from 'src/app/components/payment/payment.component';
-import { StudentenrollcoursesComponent } from 'src/app/components/studentenrollcourses/studentenrollcourses.component';
 import { SubjectComponent } from 'src/app/components/subject/subject.component';
 import { TeachercourseviewComponent } from 'src/app/components/teachercourseview/teachercourseview.component';
 
@@ -24,7 +23,6 @@ const routes: Routes = [
           { path: 'class', component: ClassComponent },
           { path: 'courses', component: CoursesComponent },
           { path: 'teachercourseview', component: TeachercourseviewComponent },
-          { path: 'studentenrollcourses', component: StudentenrollcoursesComponent },
         ]
       }
     ]
